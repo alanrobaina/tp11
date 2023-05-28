3f4390f84eabff65148b3d43e1c4125dee06abdf
alanrobaina <alanlakd@outlook.com>
2023-05-28 20:02:27 -0300

    trabajo practico 11

---
+++ tp11.js
@@ -0,0 +1,54 @@
+// // contador while
+
+// let count = 0;
+// while (count < 11) {
+//  console.log("numero es mas chico que once " + count);
+//  count = count +1}
+
+
+// //  ejercio heladeria
+
+// var topping = "kitkat";
+
+// if (topping == "oreo"){
+//     document.write("el precio es 10");}
+// else if  (topping == "kitkat"){   
+//     document.write("el precio es 15");}
+// else if  (topping == "kinder"){     
+//     document.write("el precio es 25");} 
+    
+//     else {
+//     document.write("No tenemos este topping");
+// }
+
+// // se suma
+// const oreo = 10
+// const kitkat = 15
+// const kinder = 25
+// const helado = 550
+// var preciofinal = vsuma + const 
+
+// document.write("la suma es: " + vsuma);
+// document.write(" la suma de la suma es: " + vvsuma);
+
+
+// // ejercicio for 
+// const dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+// for(let i=3; i < dias.length; i--){
+// dias.push("libre");
+// console.log("El elemento es: " + dias[i])
+// }
+
+// ejercico switch
+
+let pedido = "carne"; "pescado"; "verdura"; 
+
+switch(pedido){
+case carne: console.log("tome vino tinto ");
+break;
+case pescado: console.log("tome vino blanco");
+break;
+case verdura: console.log("tome vino blanco");
+break;
+default: console.log("elija carne, pescado o verdura");
+}
